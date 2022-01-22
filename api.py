309d8b1840c16825fcab565b2abc804f5e8db781
karun <mailto.karun.py@gmail.com>
2022-01-22 23:45:03 +0530

    ADDED: Documentation Documented Code

--- api.py
+++ api.py
@@ -1,30 +1,45 @@
+"""------------Importing Dependencies Start-----------"""
+# Imported Sanic, Asynchronous web frameworks
 from sanic import Sanic, json
+# Imported AIOHttp, Asynchronous HTTP Client
 from aiohttp import ClientSession
-
+# Imported ping to play with websites
 from ping import ping_url
+"""-----------Importing Dependencies End---------------"""
 
+"""-------------Sanic App Start----------"""
+# Created Sanic Application
 app = Sanic("PeakAPI")
 
+
+# Created a client session
 @app.listener('before_server_start')
 async def init(app, loop):
     app.ctx.aiohttp_session = ClientSession(loop=loop)
 
+
+# Listener to close session
 @app.listener('after_server_stop')
 def finish(app, loop):
     loop.run_until_complete(app.ctx.aiohttp_session.close())
     loop.close()
 
+
+# Main/Home Path
 @app.get('/')
 async def index(request):
     return json({'message': 'Peak API says hello!'})
 
+
+# Created /ping path to get url
 @app.get('/ping')
+# Creating /url parameter
 async def ping(request):
     url = request.args.get("url")
-
     result = await ping_url(url, app.ctx.aiohttp_session)
-
     return json(result)
+"""-----------Sanic App End-----------"""
 
+# Let's run
 if __name__ == "__main__":
     app.run()
--- ping.py
+++ ping.py
@@ -1,10 +1,12 @@
 from aiohttp.client_exceptions import ClientConnectorCertificateError, ClientConnectorError, InvalidURL
 
 
+# Outputs
 async def ping_url(url, session):
     try:
         async with session.get(url) as resp:
             return {"message": resp.status}
+# Handling Errors
     except ClientConnectorCertificateError:
         return {"error": "SSLError"}
     except InvalidURL:
@@ -12,4 +14,4 @@ async def ping_url(url, session):
     except ClientConnectorError:
         return {"error": "ClientConnectorError"}
     except TypeError:
-        return {"error":"NoInput" }
+        return {"error": "NoInput"}
