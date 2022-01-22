43d32cee62e83b44d4db5668a913e76093c30dac
SyedAhkam <smahkam57@gmail.com>
2022-01-22 21:34:54 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from PyQt5.QtWidgets import QApplication
+
+import sys
+
+from window import PeakWindow
+
+if __name__ == "__main__":
+    # Create a QApplication
+    app = QApplication(sys.argv)
+
+    # Create a window
+    window = PeakWindow()
+
+    # Show window
+    window.show()
+
+    # Handle process exit
+    sys.exit(app.exec_())
---
+++ window.py
@@ -0,0 +1,22 @@
+from PyQt5.QtWidgets import QMainWindow
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 500
+
+class PeakWindow(QMainWindow):
+    """Main Application subclass of `QApplication`"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes our window"""
+
+        # Init parent class
+        super().__init__(
+            *args,
+            **kwargs
+        )
+
+        # Set window title
+        self.setWindowTitle("Peak")
+        
+        # Set window size
+        self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
