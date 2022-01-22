49d322e0e7c2984f178776c24d728e30fe5d75ef
SyedAhkam <smahkam57@gmail.com>
2022-01-22 22:04:54 +0530

    Add ping endpoint

--- api.py
+++ api.py
@@ -6,5 +6,11 @@ app = Sanic("PeakAPI")
 async def index(request):
     return json({'message': 'Peak API says hello!'})
 
+@app.get('/ping')
+async def ping(request):
+    url = request.args.get("url")
+
+    return json({'message': url})
+
 if __name__ == "__main__":
     app.run()
