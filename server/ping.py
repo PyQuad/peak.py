75976567a34ee38462fe23df4c22db10d4861c33
Syed Ahkam <smahkam57@gmail.com>
2022-02-06 18:59:02 +0530

    Create class Pinger

--- server/ping.py
+++ server/ping.py
@@ -4,56 +4,48 @@ from aiohttp.client_exceptions import ClientConnectorCertificateError, ClientCon
 
 from models import WebLog
 
-current_ping = 0
+class Pinger:
+    def __init__(self):
+        self.current_ping = 0
+    
+    async def save_log(self, **kwargs):
+        return await WebLog.create(**kwargs)
 
+    async def on_request_start(self, session, trace_config_ctx, params):
+        self.current_ping = 0  # reset on every request
+        trace_config_ctx.start = asyncio.get_event_loop().time()
 
-async def save_log(**kwargs):
-    return await WebLog.create(**kwargs)
+    async def on_request_end(self, session, trace_config_ctx, params):
+        self.current_ping = asyncio.get_event_loop().time() - trace_config_ctx.start
+        print("Request took {}".format(self.current_ping))
 
+    async def ping_url(self, url, session):
+        resp = None
+        try:
+            resp = await session.get(url)
+            await resp.read() # reading the contents is critical to the actual response times
 
-async def on_request_start(session, trace_config_ctx, params):
-    current_ping = 0  # reset on every request
-    trace_config_ctx.start = asyncio.get_event_loop().time()
+            # Return success response
+            return {"message": {"status": resp.status, "reason": resp.reason, "ping": self.current_ping}}
 
+        # Handling Errors
+        except ClientConnectorCertificateError:
+            return {"error": "SSLError"}
+        except InvalidURL:
+            return {"error": "InvalidURL"}
+        except ClientConnectorError:
+            return {"error": "ClientConnectorError"}
+        except TypeError:
+            return {"error": "NoInput"}
+        finally:
+            if resp:
+                # Save a web log
+                await self.save_log(
+                    site_addr=resp.url,
+                    ping_ms=self.current_ping,
+                    status=resp.status,
+                    reason=resp.reason
+                )
 
-async def on_request_end(session, trace_config_ctx, params):
-    current_ping = asyncio.get_event_loop().time() - trace_config_ctx.start
-    print("Request took {}".format(current_ping))
-
-
-
-
-
-# Outputs
-async def ping_url(url, session):
-    resp = None
-    try:
-        resp = await session.get("https://saharaa.in")
-        #rp = await resp.read()
-        #print(rp)
-
-        # Return success response
-        return {"message": {"status": resp.status, "reason": resp.reason}}
-
-    # Handling Errors
-    except ClientConnectorCertificateError:
-        return {"error": "SSLError"}
-    except InvalidURL:
-        return {"error": "InvalidURL"}
-    except ClientConnectorError:
-        return {"error": "ClientConnectorError"}
-    except TypeError as e:
-        print(e)
-        return {"error": "NoInput"}
-    finally:
-        if resp:
-            # Save a web log
-            await save_log(
-                site_addr=resp.url,
-                ping_ms=current_ping,
-                status=resp.status,
-                reason=resp.reason
-            )
-
-            # Close the response
-            resp.close()
+                # Close the response
+                resp.close()
