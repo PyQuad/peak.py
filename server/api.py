a04f05c27ac7c2b39132c29349dbdce70d5e0e52
Syed Ahkam <smahkam57@gmail.com>
2022-02-06 19:45:26 +0530

    Use Pinger class

--- server/api.py
+++ server/api.py
@@ -1,8 +1,10 @@
 """------------Importing Dependencies Start-----------"""
 import logging
+
 # Imported Sanic, Asynchronous web frameworks
 import aiohttp
 from sanic import Sanic, json
+
 # Imported AIOHttp, Asynchronous HTTP Client
 from aiohttp import ClientSession
 
@@ -12,12 +14,12 @@ from tortoise_config import TORTOISE_ORM
 from models import WebLog
 
 # Imported ping to play with websites
-from ping import ping_url, on_request_start, on_request_end
+from ping import Pinger
 
 # Setup logging
 logging.basicConfig(level=logging.DEBUG)
 
-"-----------Importing Dependencies End---------------"""
+"-----------Importing Dependencies End---------------" ""
 
 """-------------Sanic App Start----------"""
 # Created Sanic Application
@@ -25,42 +27,43 @@ app = Sanic("PeakAPI")
 
 
 # Created a client session
-@app.listener('before_server_start')
+@app.listener("before_server_start")
 async def init(app, loop):
+    # Bind the pinger class
+    app.ctx.pinger = Pinger()
+
+    # Bind the trace config
     trace_config = aiohttp.TraceConfig()
-    trace_config.on_request_start.append(on_request_start)
-    trace_config.on_request_end.append(on_request_end)
+    trace_config.on_request_start.append(app.ctx.pinger.on_request_start)
+    trace_config.on_request_end.append(app.ctx.pinger.on_request_end)
     app.ctx.aiohttp_session = ClientSession(loop=loop, trace_configs=[trace_config])
 
-
 # Listener to close session
-@app.listener('after_server_stop')
+@app.listener("after_server_stop")
 def finish(app, loop):
+    # Close aiohttp loop on server stop
     loop.run_until_complete(app.ctx.aiohttp_session.close())
     loop.close()
 
 
 # Main/Home Path
-@app.get('/')
+@app.get("/")
 async def index(request):
-    return json({'message': 'Peak API says hello!'})
+    return json({"message": "Peak API says hello!"})
 
 
 # Created /ping path to get url
-@app.get('/ping')
+@app.get("/ping")
 # Creating /url parameter
 async def ping(request):
     url = request.args.get("url")
-    result = await ping_url(url, app.ctx.aiohttp_session)
+    result = await app.ctx.pinger.ping_url(url, app.ctx.aiohttp_session)
     return json(result)
+
 
 """-----------Sanic App End-----------"""
 
-register_tortoise(
-    app,
-    config=TORTOISE_ORM,
-    generate_schemas=True
-)
+register_tortoise(app, config=TORTOISE_ORM, generate_schemas=True)
 
 # Let's run
 if __name__ == "__main__":
