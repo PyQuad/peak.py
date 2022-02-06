36d4e64b3ccf3f2c3895a4a0da6753c507ff9ec9
Syed Ahkam <smahkam57@gmail.com>
2022-02-06 20:25:17 +0530

    Increase max chars in reason field

--- server/models.py
+++ server/models.py
@@ -5,5 +5,5 @@ class WebLog(Model):
     site_addr = fields.CharField(max_length=256)
     ping_ms = fields.FloatField()
     status = fields.SmallIntField()
-    reason = fields.CharField(16)
+    reason = fields.CharField(max_length=64)
     checked_at = fields.DatetimeField(auto_now_add=True)
